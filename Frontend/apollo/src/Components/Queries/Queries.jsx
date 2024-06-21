58b8cc9a5d81c16e442493c79326d9626ebb2544
Meghana <meghanamanchal@gmail.com>
2024-06-21 18:26:35 +0530

    added responsive design

--- Frontend/apollo/src/Components/Queries/Queries.jsx
+++ Frontend/apollo/src/Components/Queries/Queries.jsx
@@ -9,21 +9,21 @@ function Queries() {
       <Navbar />
       <div className='container mt-5'>
       <div className='mt-5 mb-5'>
-        <h1>Have Questions? We’re Here to Help!</h1>
-        <p>Welcome to our Queries section! If you have any questions about our services, appointment bookings, or any other inquiries, feel free to reach out to us. We’re dedicated to providing you with the information and assistance you need.</p>
+        <h1 className='question-query-page'>Have Questions? We’re Here to Help!</h1>
+        <p className='para-query'>Welcome to our Queries section! If you have any questions about our services, appointment bookings, or any other inquiries, feel free to reach out to us. We’re dedicated to providing you with the information and assistance you need.</p>
       </div>
       <div className='mt-5 mb-5'>
         <h2 className='quer'>Submit Your Query:</h2>
-        <p>Please use the form below to submit your query. Our team will review it and get back to you as soon as possible. Thank you for reaching out to us!</p>
+        <p className='para-query'>Please use the form below to submit your query. Our team will review it and get back to you as soon as possible. Thank you for reaching out to us!</p>
         <textarea className='query-textarea'>  </textarea>
         <button className='btn btn-primary submit-btn'>Submit</button>
       </div>
       <div className='mt-5 mb-5'>
-        <h2>Contact Information:</h2>
-        <p>For further assistance, you can contact us directly:</p>   
-          <li>Phone: 1234567890</li>
-          <li>Email: apollohospital@gmail.com</li>
-          <li>Office Hours: 24/7</li>
+        <h2 className='question-query-page'>Contact Information:</h2>
+        <p className='para-query'>For further assistance, you can contact us directly:</p>   
+          <li className='para-query'>Phone: 1234567890</li>
+          <li className='para-query'>Email: apollohospital@gmail.com</li>
+          <li className='para-query'>Office Hours: 24/7</li>
       </div>
       </div>
     </>
