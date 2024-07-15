18fdb4df18b6925e7bcf847597d79adda4df2f34
meghanamanchala <meghanamanchal@gmail.com>
2024-07-15 16:51:04 +0530

    added css

--- Backend/DoctorLogin/DoctorSchema.js
+++ Backend/DoctorLogin/DoctorSchema.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const doctorLoginSchema = new mongoose.Schema({
-    Docusername: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    Docpassword: {
-        type: String,
-        required: true
-    }
-});
+// const doctorLoginSchema = new mongoose.Schema({
+//     Docusername: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     Docpassword: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-const doctorDetails = mongoose.model('doctorlogin', doctorLoginSchema);
-module.exports = doctorDetails;
+// const doctorDetails = mongoose.model('doctorlogin', doctorLoginSchema);
+// module.exports = doctorDetails;
